823ed9baa53c35fd310df0492eecd2f2ec3786fb
rumesaliaquat <rumesaliaquat@gmail.com>
2021-12-18 17:44:48 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// 
+// var fname = prompt("enter your firstname");
+// var lname = prompt("enter your lastname");
+// var fullName = fname + lname;
+// alert("Hello " + fullName);
+
+// var favPhoneModel = prompt("enter your favorite mobile model");
+// document.write("My favorite phone is: " + favPhoneModel + "<br>");
+// var x = favPhoneModel.length;
+// document.write("Length of string: " + x);
+
+// var a = "Pakistani";
+// var x = a.indexOf("n");
+// document.write("Index of 'n': " + x);
+
+// var a = "Hello world";
+// var x = a.lastIndexOf("l");
+// document.write(x);
+
+// var a = "Pakistani";
+// var x = a.charAt(3);
+// document.write(x);
+
+// var a = "Hyderabad";
+// document.write("City: " + a + "<br>");
+// var x = a.replace("Hyder", "Islam");
+// document.write("After replacement: " + x);
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var x = message.replace(/and/g, "&");
+// document.write(x);
+
+// var a = "472";
+// var convertInNum = Number(a);
+// document.write(convertInNum + "<br>" + typeof(convertInNum));
+ 
+// var a = prompt("enter any name");
+// document.write("User Input: " + a + "<br>");
+// var x = a.toUpperCase();
+// document.write("Convert to uppercase: " + x);
+
+// var a = prompt("enter some text");
+// var firstLetter = a.slice(0,1);
+// var firstLetter = firstLetter.toUpperCase();
+// var otherLetter = a.slice(1);
+// var otherLetter = otherLetter.toLowerCase();
+// var x = firstLetter + otherLetter;
+// document.write("Change in title case: " + x);
+
+// var num = 35.36;
+// var x = num.toString();
+// document.write("convert into string: " + x +"<br>"+ typeof(x));
+// // var z = parseInt(num);
+// // document.write(z);
+
+// 
+// var num = +prompt("enter any positive integer.");
+// document.write("number: " + num + "<br>");
+// var x = Math.round(num);
+// document.write("round off value: " + x + "<br>");
+// var y = Math.floor(num);
+// document.write("floor value: " + y + "<br>");
+// var z = Math.ceil(num); 
+// document.write("ceil value: " + z);
+
+// var num = +prompt("enter any negative float number.");
+// document.write("number: " + num + "<br>");
+// var x = Math.round(num);
+// document.write("round off value: " + x + "<br>");
+// var y = Math.floor(num);
+// document.write("floor value: " + y + "<br>");
+// var z = Math.ceil(num); 
+// document.write("ceil value: " + z);
+
+// var num = -787;
+// var x = Math.abs(num);
+// document.write(x);
+
+// var diceNum = Math.random() * 7;
+// document.write("Random dice value: " + diceNum + "<br>");
+// var x = parseInt(diceNum);
+// document.write("dice value convert in integer: " + x + "<br>");
+// //OR
+// var x = Math.round(diceNum);
+// document.write("Round off value of dice: " + x);
+
+// var coinNum = Math.random() * 2;
+// document.write("random coin value: " + coinNum + "<br>");
+// var x = parseInt(coinNum);
+// document.write("integer value: " + x + "<br>");
+// if(x === 0){
+//     document.write("toss value:  Heads");
+// }
+// else{
+//     document.write("toss value: Tails");
+// }
+
+// 
+// var date = new Date();
+// document.write(date);
+
+// var date = new Date();
+// document.write(date + "<br>");
+// var x = date.getMonth();
+// document.write(x);
+
